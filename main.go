8849bfeb5326d1230ac700a4941747766a67f7ab
Mike O'Toole <mo@finxact.com>
2020-02-19 23:07:00 -0500

    intial project

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.DebugLevel)
+}
+
+func main() {
+
+	log.Info("Simple Test")
+}
